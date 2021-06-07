2502ac07e06de826e8709848271c31cb7ba7499d
Daniel González <dgtrabada@gmail.com>
2021-06-07 17:02:05 +0200

    listas

--- 01_Tipos/tipos.py
+++ 01_Tipos/tipos.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 
-print ("Hola Mundo")
+# Tipos básicos :
+#  Números
+#  Cadenas
+#  Booleanos
+
+a=5
+b=3.14
+c=3.14e-3
+d=2.1+7.8j
+e="Hola Mundo"
+f=True
+
+print (a," es un entero ",type(a))
+print (b," es un float ",type(b))
+print (c," es un float notación cientifica ",type(c))
+print (d," es un complejo ",type(d))
+print (e," es una cadena ",type(e))
+print (f," es un booleano ",type(f))
+
+
+
