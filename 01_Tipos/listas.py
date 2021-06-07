7c84172be00552b92bcef4c8b0ed74afd3f6f35e
Daniel González <dgtrabada@gmail.com>
2021-06-07 17:03:18 +0200

    listas

---
+++ 01_Tipos/listas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# La lista es un tipo de colección ordenada
+
+l = [ 3.14, False, "esto es una cadena", [2,"f"]]
+
+print("podemos ver la lista")
+print(l)
+print("podemos escribir el primer elemento de la lista")
+print(l[0])
+print("podemos cambiar el segundo elemento de la lista")
+l[1]=33
+print(l)
+print("poemos seleccionar una porciones de la lista")
+new_l1 = l[0:2]
+print(new_l1)
